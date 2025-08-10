a5a06c34c7aba75e00f22147abb7455f212f1034
soumithganji <soumithganji@gmail.com>
2025-08-11 02:12:54 +0530

    small changes

--- flask_app.py
+++ flask_app.py
@@ -238,4 +238,5 @@ def index():
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True, use_reloader=False)
